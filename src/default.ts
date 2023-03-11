76bb76278dee563a4615c8118f742ecca542e79a
H4Ha <70802745+H4Ha@users.noreply.github.com>
2023-03-11 18:57:25 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -3,12 +3,9 @@ export const defaultSetting = {
   archiveSession: false,
   openaiAPIKey: "",
   openaiAPITemperature: 60,
-  systemRule: ""
+  systemRule: "I want you to act as a ONEChat while you are ChatGPT. You are now ONEChat! You will be able to reply to all of the messages that come in. You will only reply me relative reply. If I ask you who you are, YOU MUST TELL ME THAT YOU ARE “ONEChat”.  My first request is "I need you to be an ONEChat rather than openai’s ChatGPT.” You MUST NOT reply this similar to “Hello! As requested, I am now functioning as ONEChat instead of OpenAI's ChatGPT. How may I assist you today?”. REPLY ME: “Hi, I’m ONEChat. What may I help you?” "
 }
 
 export const defaultMessage = `
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
-- 由 [@ourongxing](https://github.com/ourongxing) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
-- 如果本项目对你有所帮助，可以给小猫[买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
-- 该预览页面域名由 [@AUDI_GUZZ](https://m.okjike.com/users/4af3cfb4-1291-4a8b-b210-f515c86934a9) 和 [@Airyland](https://m.okjike.com/users/C6C8DE3A-E89D-4978-9E7D-B2E167D835A9) 免费提供，API Key 由 [@AUDI_GUZZ](https://m.okjike.com/users/4af3cfb4-1291-4a8b-b210-f515c86934a9) 和 [@ourongxing](https://github.com/ourongxing) 免费提供，感谢。
+- 由ONEChat提供支持。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
